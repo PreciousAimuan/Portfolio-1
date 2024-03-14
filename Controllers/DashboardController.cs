356da793d8c52ee834fb5e8fc26891d8e412e878
Precious Aimuan <preciousaimuan@decagon.dev>
2024-03-14 16:26:06 +0100

    new update

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SQ20.Net_Wee7_8_Task.Models;
+using System.Diagnostics;
 
 namespace SQ20.Net_Wee7_8_Task.Controllers
 {
-    public class DashboardController : Controller
+    //[Authorize(Roles = "Admin")]
+    public class DashBoardController : Controller
     {
+        private readonly ILogger<DashBoardController> _logger;
+
+        public DashBoardController(ILogger<DashBoardController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- obj/Debug/net6.0/SQ20.Net_Wee7-8_Task.AssemblyInfo.cs
+++ obj/Debug/net6.0/SQ20.Net_Wee7-8_Task.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQ20.Net_Wee7-8_Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801d1df8b8048669667bf4879d5740f4458182ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1643934abecd6aca8ab7a2c5d8aa46eb689bed74")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQ20.Net_Wee7-8_Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQ20.Net_Wee7-8_Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
